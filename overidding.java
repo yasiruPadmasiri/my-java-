b75a7ed56740310a9a65656a62db25b49b17f253
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-10-30 17:38:51 +0530

    overide super key

--- overidding.java
+++ overidding.java
@@ -1,26 +1,50 @@
 class student{
+    int x;
+    student(){
+        System.out.println("Student is created");//constructor
+    }
+    student(int x){
+        this.x=x;
+        System.out.println("subject is created "+x);
+    }
     void display(){
         System.out.println("this is student");
     }
 }
 class subject extends student{
+    int y;
+    subject(int y){
+        this.y=y;
+        System.out.println("subject is created "+y);
+    }
+    subject(){
+        System.out.println("subject is created "+y);//constructor
+    }
+
     void display(){
         System.out.println("this is subject class");
     }
 }
 class sport extends student{
+    sport(){
+        System.out.println("sport is created");//constructor
+    }
+
     void display(){
         System.out.println("this is sport class");
+        super.display();//super keyword using here
     }
 }
 class overide{
     public static void main(String args[]){
-        student std=new student();
-        subject sbj=new subject();
+        student std=new student(10);
+        subject sbj=new subject(8);
         sport spr=new sport();
 
+        System.out.println("\n");
         std.display();
         sbj.display();
         spr.display();
+        
     }
 }
